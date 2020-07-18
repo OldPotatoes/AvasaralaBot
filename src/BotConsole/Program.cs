287a67f9b779826794c7a7bbc68d3196d77f98a2
Jamie Dyer <oldpotatoes@jamiedyer.com>
2020-07-18 17:09:57 -0400

    Initial code

---
+++ src/BotConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BotTweeter;
+
+namespace BotConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var tweeter = new BotTweeter.Tweeter();
+            tweeter.Menu();
+        }
+    }
+}
