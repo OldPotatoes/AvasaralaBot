1310d24df48ea04bc6c7fdc95a0302f756486e8b
LAPTOP-CKB2H4TA\alexa <alexandercdyer@gmail.com>
2020-07-29 15:08:35 -0400

    New Test area

--- src/BotSqlite/DBAccess.cs
+++ src/BotSqlite/DBAccess.cs
@@ -1,18 +1,51 @@
+using System;
 using Dapper;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 
-namespace APCSP_Final_Project
+namespace BotSqlite
+
 {
-    class DatabaseAccess
+    public class DatabaseAccess
     {
         private SQLiteConnection connection;
         private string dbConnectionString; // = @"Data Source = D:\Code\WisdomOfAvasarala.db";
 
-        public TestDbProcess(String DbConn)
+        public DatabaseAccess(string DbConn)
         {
             dbConnectionString = DbConn;
+            connection = new SQLiteConnection(dbConnectionString);
         }
+                public long CountNumQuote()
+        {
+            string sql = "SELECT COUNT(*) FROM WisdomOfAvasarala";
+            long result = connection.Query<long>(sql).First();
+            return result;
+        }
+        public List<Quote> GetQuote(long count = 100)
+        {
+            string sql = "SELECT Book, Chapter, Page, Quote FROM WisdomOfAvasarala LIMIT @Count";
+            List<Quote> result = connection.Query<Quote>(sql, new { Count = count }).AsList();
+            return result;
+        }
+        public List<Quote> GetSpecificQuote(int indexQueryInt)
+        {
+            string sql = "SELECT Book, Chapter, Page, Quote FROM WisdomOfAvasarala WHERE Id = @Index";
+            List<Quote> result = connection.Query<Quote>(sql, new { Index = indexQueryInt }).AsList();
+            return result;
+        }
+        public string GetRandomQuote()
+        {
+            string maxSql = "SELECT MAX(Id) FROM WisdomOfAvasarala";
+            int max = connection.Query<int>(maxSql).First();
+            var rand = new Random();
+            string sql = "SELECT Book, Chapter, Page, Quote FROM WisdomOfAvasarala WHERE Id = @Rand";
+            int randNum = rand.Next(max);
+            var result = connection.Query<Quote>(sql, new { Rand = randNum });
+            Quote resultQuote = result.AsList()[0];
+            return resultQuote.quote;
+        }
+
     }
 }
