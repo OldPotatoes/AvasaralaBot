08c620d35f468670d8f67f28ceaab3435149e2c6
Jamie Dyer <oldpotatoes@jamiedyer.com>
2020-08-05 13:55:09 -0400

    Missed file from 'Added appsettings.json'

--- src/BotConsole/Program.cs
+++ src/BotConsole/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using BotTweeter;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BotConsole
 {
     class Program
     {
+        public static IConfigurationRoot Configuration  { get; private set; }
+
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
-            var menu = new Menu();
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            String DbConn = Configuration.GetConnectionString("DataConnection");
+
+            // IConfigurationSection listOfEmails = Configuration.GetSection("EmailAddresses");
+            // foreach (var email in listOfEmails.AsEnumerable())
+            //     Console.WriteLine($"{email.Key}:{email.Value}");
+
+            var menu = new Menu(DbConn);
             menu.ShowMenu();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(Configuration);
         }
     }
 }
