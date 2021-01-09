dd516ee7aa86d06e3db01351c01776cf0d198910
Jamie Dyer <oldpotatoes@jamiedyer.com>
2021-01-09 15:04:54 -0500

    Revert "AWS added"
    This reverts commit bf9b463d384563265707e930fa490aab1081228c.
    
    Hopefully.

--- src/BotService/Worker.cs
+++ src/BotService/Worker.cs
@@ -11,11 +11,6 @@ using BotTweeter;
 
 namespace BotService
 {
-    const int SECOND = 1000;
-    const int MINUTE = SECOND * 60;
-    const int HOUR = MINUTE * 60;
-    const int DAY = HOUR * 24;
-
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
@@ -47,7 +42,7 @@ namespace BotService
                 var tweeter = new Tweeter(); 
                 tweeter.MaybeTweet(tweetText, actuallyTweeting);
 
-                await Task.Delay(12*HOUR, stoppingToken);
+                await Task.Delay(10000, stoppingToken);
             }
         }
 
