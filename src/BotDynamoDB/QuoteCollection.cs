e826d2b545dd196c34a540928a69bda0fc96ef20
Jamie Dyer <oldpotatoes@jamiedyer.com>
2021-01-09 16:50:42 -0500

    aws

---
+++ src/BotDynamoDB/QuoteCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotDynamoDB
+{
+    public class QuoteCollection
+    {
+        public IEnumerable<Quote> Quotes { get; set; }
+        public string PaginationToken { get; set; }
+    }
+}
